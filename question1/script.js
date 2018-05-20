351c22ebab6f26417d6ece1ba2eef4fceebc1c0d
Maren Grimstad <36054558+maren793@users.noreply.github.com>
2018-05-20 23:52:38 +0200

    Add files via upload

---
+++ question1/script.js
@@ -0,0 +1,14 @@
+//Use RegEx to validate form
+function validatePhone() {
+var phone = /[0-9]/;
+
+}
+
+function validateEmail(elementValue){
+    // create a pattern to test an email address
+    var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    
+function validateName() {
+    var namePattern = / /
+}
+
